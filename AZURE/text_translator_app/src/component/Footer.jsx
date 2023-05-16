8e6f47b17efb43d3164f09dc2c10082de4c0d613
SAHIL PAL <69889824+sahil-pal@users.noreply.github.com>
2023-05-16 11:59:57 +0530

    Delete AZURE directory

--- AZURE/text_translator_app/src/component/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () =>{
-    return (
-        <div className="row justify-center">
-            <h3>Created by Team ( Sahil Pal, Sahil Bhatia & Raghavendra Singh )</h3>
-        </div>
-    )
-}
